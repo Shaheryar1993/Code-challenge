71f703c33baeba589874ad2eeef3dc5f33e2efec
Muhammad Shaheryar <Shaheryar.shakaib@outlook.com>
2021-08-27 15:13:20 +0200

    the updated work

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -7,8 +7,10 @@ using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsInput;
 
 namespace WindowsFormsApp1
 {
@@ -21,12 +23,13 @@ namespace WindowsFormsApp1
         [DllImport("user32.dll")]
         static extern bool SetForegroundWindow(int hWnd);
 
-    
-        [DllImport("user32.dll", EntryPoint = "SendMessageA")]
-        private static extern int SendMessage(int hwnd, int wMsg, int wParam, string lParam);
-        [DllImport("user32.dll", EntryPoint = "SetWindowText", CharSet = CharSet.Ansi)]
-        public static extern bool SetWindowText(IntPtr hWnd, String strNewWindowName);
-        private const int EM_SETCUEBANNER = 0x1501;
+        [DllImport("user32.dll")]
+        static extern IntPtr SetActiveWindow(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(int hWnd, IntPtr msg, int wParam, int lParam);
+
+
+        public const int WM_SETTEXT = 0x1501;
 
 
         //using spy++ to get the handler of the repective window and field
@@ -34,24 +37,28 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
-            int CHand = 0;
-            int PHand = FindWindow("CabinetWClass", "File Explorer");
-            CHand = FindWindowEx(PHand, 0, "WorkerW", null);
-            CHand = FindWindowEx(CHand, 0, "ReBarWindow32", null);
-            CHand = FindWindowEx(CHand, 0, "UniversalSearchBand", null);
-            CHand = FindWindowEx(CHand, 0, "ModernSearchBox", "SearchEditBox");
+            int ChildHand = 0;
+            int ParentHand = FindWindow("CabinetWClass", "File Explorer");
+            ChildHand = FindWindowEx(ParentHand, 0, "WorkerW", null);
+            ChildHand = FindWindowEx(ChildHand, 0, "ReBarWindow32", null);
+            ChildHand = FindWindowEx(ChildHand, 0, "UniversalSearchBand", null);
+            ChildHand = FindWindowEx(ChildHand, 0, "ModernSearchBox", "SearchEditBox");
+            
 
 
-            if (PHand != 0 & CHand != 0)
+            if (ParentHand != 0 & ChildHand != 0)
             {
-                 SetForegroundWindow(CHand);
-
-                 SendMessage(CHand, EM_SETCUEBANNER, 0, "Search Me");
+                SetForegroundWindow(ChildHand);
+                //Thread.Sleep(2000);
+                SendKeys.SendWait("Search me");
+                // this is just printing last letter of the string i.e "e" tried to find out alternative but couldnt found any.
+                //Even it some time works and print the full text
             }
             else
             {
-                MessageBox.Show("Error");
+                MessageBox.Show("Please open file explorer");
             }
         }
+
     }
 }
